96dd57cc1a34c432c7bfeb4870e2f3c75ec31420
Steven Lotito <steven.lotito@gmail.com>
2023-11-30 22:11:43 -0800

    day 1 part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,36 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let digits: Vec<_> = line.chars().filter(|c| c.is_numeric()).collect();
+                format!("{}{}", digits[0], digits[digits.len() - 1])
+                    .parse::<u32>()
+                    .unwrap()
+            })
+            .sum(),
+    )
+}
+
+pub fn part_two(_: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(142));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
