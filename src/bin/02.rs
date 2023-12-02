9bc5c7ac34397b506e33e8dda617acd8e7c63dc7
Steven Lotito <steven.lotito@gmail.com>
2023-12-02 09:37:36 -0800

    minor refactoring

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,20 +1,64 @@
 advent_of_code::solution!(2);
 
 use regex::Regex;
+use std::cmp;
 
-#[derive(Debug)]
+#[derive(Default, Debug, Clone)]
 pub struct Cubes {
-    r: u8,
-    g: u8,
-    b: u8,
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Cubes {
+    fn max(&mut self, other: &Cubes) {
+        self.red = cmp::max(self.red, other.red);
+        self.green = cmp::max(self.green, other.green);
+        self.blue = cmp::max(self.blue, other.blue);
+    }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let summed_ids: u32 = extract_games(input)
+        .iter()
+        .filter_map(|(id, sets)| {
+            if sets.iter().all(|cube| is_valid_set(cube)) {
+                Some(*id as u32)
+            } else {
+                None
+            }
+        })
+        .sum();
+    Some(summed_ids)
+}
+
+fn is_valid_set(cubes: &Cubes) -> bool {
+    cubes.red <= 12 && cubes.green <= 13 && cubes.blue <= 14
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let power: u32 = extract_games(input)
+        .iter()
+        .map(|(_id, sets)| {
+            let mut acc = Cubes::default();
+            let cube = sets.iter().fold(&mut acc, |acc, this_set| {
+                acc.max(this_set);
+                acc
+            });
+            cube.clone()
+        })
+        .map(|c| c.red as u32 * c.green as u32 * c.blue as u32)
+        .sum();
+    Some(power)
+}
+
+fn extract_games(input: &str) -> Vec<(u8, Vec<Cubes>)> {
+    // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     let re = Regex::new(r"Game (\d+):.*").unwrap();
-    let games: u32 = input
+    input
         .lines()
         .map(|line| {
-            let id = re
+            let game_id = re
                 .captures(line)
                 .unwrap()
                 .get(1)
@@ -23,48 +67,30 @@ pub fn part_one(input: &str) -> Option<u32> {
                 .parse::<u8>()
                 .unwrap();
             let sets_line = line.split(":").skip(1).collect::<String>();
+            //  3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
             let sets: Vec<_> = sets_line
                 .trim()
                 .split(";")
                 .map(|part| {
-                    let mut c = Cubes { r: 0, g: 0, b: 0 };
-                    // 8 green, 6 blue, 20 red
-                    part.split(", ").for_each(|rgb| {
-                        if rgb.contains(" green") {
-                            c.g = rgb.replace(" green", "").trim().parse().unwrap();
-                        } else if rgb.contains(" blue") {
-                            c.b = rgb.replace(" blue", "").trim().parse().unwrap();
-                        } else if rgb.contains(" red") {
-                            c.r = rgb.replace(" red", "").trim().parse().unwrap();
+                    let mut cubes = Cubes::default();
+                    // 3 blue, 4 red
+                    part.split(", ").for_each(|set| {
+                        if set.contains(" green") {
+                            cubes.green = set.replace(" green", "").trim().parse().unwrap();
+                        } else if set.contains(" blue") {
+                            cubes.blue = set.replace(" blue", "").trim().parse().unwrap();
+                        } else if set.contains(" red") {
+                            cubes.red = set.replace(" red", "").trim().parse().unwrap();
                         } else {
-                            panic!("could not parse values from set, line is: {rgb}");
+                            panic!("could not parse values from set, line is: {set}");
                         }
                     });
-                    c
+                    cubes
                 })
                 .collect();
-            (id, sets)
+            (game_id, sets)
         })
-        // .inspect(|game| {
-        //     dbg!(game);
-        // })
-        .filter_map(|(id, sets)| {
-            if sets.iter().all(|cube| is_valid_set(cube)) {
-                Some(id as u32)
-            } else {
-                None
-            }
-        })
-        .sum();
-    Some(games)
-}
-
-fn is_valid_set(cubes: &Cubes) -> bool {
-    cubes.r <= 12 && cubes.g <= 13 && cubes.b <= 14
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+        .collect()
 }
 
 #[cfg(test)]
@@ -80,6 +106,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2286));
     }
 }
