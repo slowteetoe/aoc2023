641ad7b12418713ef08b7b6f559df1de13562806
Steven Lotito <steven.lotito@gmail.com>
2023-12-03 17:03:10 -0800

    day 3 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,24 +1,40 @@
 advent_of_code::solution!(3);
-use std::ops::Deref;
+use std::{collections::BTreeMap, ops::Deref};
 
 #[derive(Debug)]
 struct Grid(Vec<Vec<char>>);
 impl Grid {
     fn is_symbol(&self, x: isize, y: isize) -> bool {
         if x < 0 || y < 0 {
-            // println!("ignoring [{}][{}], outside grid", x, y);
             return false;
         }
         let x = x as usize;
         let y = y as usize;
 
         if x > self[0].len() - 1 || y > self.len() - 1 {
-            // println!("ignoring [{}][{}], outside grid", x, y);
             false
         } else {
             let c = self[y][x];
-            // println!("grid[{}][{}] = '{}'", x, y, &c);
             c != '.' && !c.is_numeric()
+        }
+    }
+
+    fn is_gear_symbol(&self, x: isize, y: isize) -> Option<(usize, usize)> {
+        if x < 0 || y < 0 {
+            return None;
+        }
+        let x = x as usize;
+        let y = y as usize;
+
+        if x > self[0].len() - 1 || y > self.len() - 1 {
+            None
+        } else {
+            // println!("grid[{}][{}] = '{}'", x, y, &c);
+            if self[y][x] == '*' {
+                Some((x, y))
+            } else {
+                None
+            }
         }
     }
 }
@@ -43,14 +59,8 @@ pub fn part_one(input: &str) -> Option<u32> {
                     part_num.push(*c);
                 }
 
-                if !part_num.is_empty() {
-                    // print!("finished part num {:?}", part_num);
-                    if adj {
-                        // println!(", part is valid.");
-                        parts.push(part_num.iter().collect::<String>().parse::<u32>().unwrap());
-                    } else {
-                        // println!(", part is NOT adjacent a symbol");
-                    }
+                if !part_num.is_empty() && adj {
+                    parts.push(part_num.iter().collect::<String>().parse::<u32>().unwrap());
                 }
                 part_num.clear();
                 adj = false;
@@ -71,12 +81,68 @@ pub fn part_one(input: &str) -> Option<u32> {
             }
         })
     });
-    // dbg!(&parts);
     Some(parts.iter().sum())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let grid = parse_grid(input);
+    let mut parts: BTreeMap<(usize, usize), Vec<u32>> = BTreeMap::new(); // keep track of the gear coord, and which parts are next to it
+    grid.iter().enumerate().for_each(|(y, grid_line)| {
+        let mut attached_gear: Option<(usize, usize)> = None;
+        let mut part_num = vec![];
+        grid_line.iter().enumerate().for_each(|(x, c)| {
+            if !c.is_numeric() || x == grid_line.len() - 1 {
+                if x == grid_line.len() - 1 && c.is_numeric() {
+                    part_num.push(*c);
+                }
+
+                if !part_num.is_empty() && attached_gear.is_some() {
+                    let (x, y) = attached_gear.unwrap();
+                    let part = part_num.iter().collect::<String>().parse::<u32>().unwrap();
+                    parts.entry((x, y)).or_insert_with(Vec::new).push(part);
+                }
+                part_num.clear();
+                attached_gear = None;
+            } else if c.is_numeric() {
+                part_num.push(*c);
+                let x = x as isize;
+                let y = y as isize;
+                if attached_gear.is_none() {
+                    let checks: Vec<_> = vec![
+                        grid.is_gear_symbol(x - 1, y),
+                        grid.is_gear_symbol(x + 1, y),
+                        grid.is_gear_symbol(x, y - 1),
+                        grid.is_gear_symbol(x, y + 1),
+                        grid.is_gear_symbol(x + 1, y + 1),
+                        grid.is_gear_symbol(x + 1, y - 1),
+                        grid.is_gear_symbol(x - 1, y - 1),
+                        grid.is_gear_symbol(x - 1, y + 1),
+                    ]
+                    .iter()
+                    .filter_map(|v| *v)
+                    .collect();
+                    if !checks.is_empty() {
+                        attached_gear = checks.first().copied();
+                        // dbg!(&attached_gear, &checks);
+                    }
+                }
+            }
+        })
+    });
+    // dbg!(&parts);
+    Some(
+        parts
+            .iter()
+            .filter_map(|(_gear_location, part_numbers)| {
+                if part_numbers.len() == 2 {
+                    // println!("Found two parts around {:?}, {} and {}", _gear_location, part_numbers[0], part_numbers[1]);
+                    Some(part_numbers[0] * part_numbers[1])
+                } else {
+                    None
+                }
+            })
+            .sum(),
+    )
 }
 
 fn parse_grid(input: &str) -> Grid {
@@ -96,6 +162,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
