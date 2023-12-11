267278e7d3a7eb76de199005989578aac751ca26
Steven Lotito <steven.lotito@gmail.com>
2023-12-10 19:30:26 -0800

    day 8 part 1, using nom

---
+++ src/bin/08.rs
@@ -0,0 +1,115 @@
+use std::collections::BTreeMap;
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::alpha1,
+    combinator::map,
+    error,
+    multi::{many1, separated_list1},
+    sequence::{preceded, tuple},
+    IResult,
+};
+
+advent_of_code::solution!(8);
+
+#[derive(Debug)]
+enum Dir {
+    L,
+    R,
+}
+
+#[derive(Debug)]
+struct InvalidDirection(String);
+
+impl TryFrom<&str> for Dir {
+    type Error = InvalidDirection;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "L" => Ok(Dir::L),
+            "R" => Ok(Dir::R),
+            _ => Err(InvalidDirection(value.to_owned())),
+        }
+    }
+}
+
+fn parse_nodes(input: &str) -> IResult<&str, Vec<(&str, &str, &str)>> {
+    preceded(
+        tag("\n\n"), // get rid of extra line
+        separated_list1(
+            // AAA = (BBB, CCC)\nBBB = (DDD, EEE)\n...
+            tag("\n"),
+            map(
+                tuple((
+                    alpha1::<&str, error::Error<_>>, // node
+                    tag(" = ("),
+                    alpha1, // left
+                    tag(", "),
+                    alpha1, // right
+                    tag(")"),
+                )),
+                |t| (t.0, t.2, t.4), // (AAA, BBB, CCC)
+            ),
+        ),
+    )(input)
+}
+
+fn parse_directions(input: &str) -> IResult<&str, Vec<Dir>> {
+    many1(map(alt((tag("L"), tag("R"))), |c| {
+        Dir::try_from(c).unwrap()
+    }))(input)
+}
+
+fn parse(input: &str) -> (Vec<Dir>, BTreeMap<&str, (&str, &str)>) {
+    let (input, directions) = parse_directions(input).unwrap();
+    let (_, nodes) = parse_nodes(input).unwrap();
+    let nodes = nodes
+        .iter()
+        .fold(BTreeMap::<&str, _>::new(), |mut acc, this_node| {
+            acc.insert(this_node.0, (this_node.1, this_node.2));
+            acc
+        });
+    (directions, nodes)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (directions, nodes) = parse(input);
+    let mut curr = "AAA";
+    let mut steps = 0;
+    let mut directions = directions.iter().cycle();
+    loop {
+        steps += 1;
+        let dir = directions.next().unwrap();
+        curr = match dir {
+            Dir::L => nodes.get(curr).unwrap().0,
+            Dir::R => nodes.get(curr).unwrap().1,
+        };
+        // println!("went {:?} to arrive at {curr}", dir);
+        if curr == "ZZZ" {
+            break;
+        }
+    }
+    Some(steps)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
