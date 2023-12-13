dc575eb40aa2903af0ae20e32eba33aa5af7f9be
Steven Lotito <steven.lotito@gmail.com>
2023-12-13 08:08:45 -0800

    day 12 wip, too high, but just fast enough to run for part 1

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,6 +1,7 @@
 use std::vec;
 
 use itertools::Itertools;
+use regex::Regex;
 
 advent_of_code::solution!(12);
 
@@ -8,33 +9,42 @@ advent_of_code::solution!(12);
 // since there are only two possiblities for each position with a question mark...
 pub fn part_one(input: &str) -> Option<u32> {
     let data = parse(input);
-    let mut answer = vec![];
-    data.iter()
-        // .take(1)
-        .for_each(|(positions, validation)| answer.push(resolve_one(&positions)));
-    dbg!(answer);
-    None
+    let a: u32 = data
+        .iter()
+        .take(2)
+        .map(|(positions, validation)| (resolve_one(&positions), build_regex(validation)))
+        .map(|(inputs, validation_regex)| {
+            let re = Regex::new(&validation_regex).unwrap();
+            inputs
+                .iter()
+                .unique()
+                .filter(|i| {
+                    if re.is_match(*i) {
+                        dbg!(&re, &i);
+                        true
+                    } else {
+                        false
+                    }
+                })
+                .count() as u32
+        })
+        .sum();
+    Some(a)
 }
 
-// fixme this is bad
-pub fn is_valid(line: &str, rules: Vec<u32>) -> bool {
-    todo!();
-    let c = line.chars().collect_vec();
-    let mut valid = true;
-    rules.iter().for_each(|n| {
-        let mut obs = 0;
-        let mut pos = 0;
-        loop {
-            if pos >= c.len() {
-                valid = false;
-                break;
-            } else if c[pos] == '.' {
-                // eat whitespace (.)
-                pos += 1;
-            } else {
-            }
+fn build_regex(rule: &Vec<u32>) -> String {
+    let mut re = r"^\.*".to_owned();
+    rule.iter().enumerate().for_each(|(pos, val)| {
+        re.push_str(r"#{");
+        re.push_str(&val.to_string());
+        re.push_str(r"}");
+        if pos == rule.len() - 1 {
+            re.push_str(r"\.*$");
+        } else {
+            re.push_str(r"\.+");
         }
     });
+    re
 }
 
 // each time, replace one placeholder with its possiblities
@@ -75,10 +85,6 @@ fn parse(input: &str) -> Vec<(&str, Vec<u32>)> {
                     .collect(),
             )
         })
-        // .map(|t| {
-        //     let v = t.0.collect();
-        //     (vec![], vec![])
-        // })
         .collect_vec()
 }
 
@@ -89,12 +95,18 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(21));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_regex_creation() {
+        let result = build_regex(&vec![1, 1, 3]);
+        assert_eq!(result, r"^\.*#{1}\.+#{1}\.+#{3}\.*$");
     }
 }
