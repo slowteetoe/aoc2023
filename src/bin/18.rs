83325b8c152d6fa616ec3469652449df75b94af9
Steven Lotito <steven.lotito@gmail.com>
2023-12-28 19:25:16 -0800

    day 18, part 1 - 215sec lol

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,12 +1,11 @@
-use std::str::FromStr;
+use std::{collections::BTreeSet, str::FromStr};
 
 use itertools::Itertools;
 use nom::{
     bytes::complete::tag,
     character::complete::{alpha1, hex_digit1, newline, space1},
-    combinator::{map, map_res, rest},
+    combinator::{map, map_res},
     multi::separated_list1,
-    number,
     sequence::{delimited, separated_pair},
     IResult,
 };
@@ -72,10 +71,93 @@ fn parse(input: &str) -> Vec<Instruction> {
     parse_lines(input).unwrap().1
 }
 
+fn display(boundary: &BTreeSet<(i32, i32)>, insides: &BTreeSet<(i32, i32)>) {
+    let min_x = boundary.iter().map(|pt| pt.0).min().unwrap();
+    let min_y = boundary.iter().map(|pt| pt.1).min().unwrap();
+    // assert!(min_x == min_y && min_x == 0, "origin is not (0,0)");
+    let max_x = boundary.iter().map(|pt| pt.0).max().unwrap();
+    let max_y = boundary.iter().map(|pt| pt.1).max().unwrap();
+    (min_x..=max_y).for_each(|y| {
+        (min_y..=max_x).for_each(|x| {
+            if boundary.contains(&(x, y)) {
+                print!("#");
+            } else if insides.contains(&(x, y)) {
+                print!("*");
+            } else {
+                print!(".");
+            }
+        });
+        println!();
+    });
+}
+
+fn fill(boundary: &BTreeSet<(i32, i32)>, start: (i32, i32)) -> BTreeSet<(i32, i32)> {
+    // just try to flood fill UDLR from a point we're hoping is inside the shape
+    let mut frontier = vec![start];
+    let mut insides = BTreeSet::new();
+    loop {
+        if frontier.is_empty() {
+            println!("done");
+            break;
+        }
+        frontier = frontier
+            .iter()
+            .map(|pt| {
+                let mut add_to_insides = BTreeSet::new();
+                let up = (pt.0, pt.1 - 1);
+                let down = (pt.0, pt.1 + 1);
+                let left = (pt.0 - 1, pt.1);
+                let right = (pt.0 + 1, pt.1);
+                let next = vec![up, down, left, right];
+                let result = next
+                    .iter()
+                    .filter(|p| {
+                        !boundary.contains(p)
+                            && !frontier.contains(p)
+                            && !insides.contains(&(p.0, p.1))
+                    })
+                    .map(|p1| {
+                        add_to_insides.insert((p1.0, p1.1));
+                        (p1.0, p1.1)
+                    })
+                    .collect_vec();
+                insides.append(&mut add_to_insides);
+                result
+            })
+            .flatten()
+            .map(|v| v)
+            .collect();
+    }
+    insides
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    use crate::Dir as D;
     let instructions = parse(input);
-    dbg!(&instructions);
-    None
+    // dbg!(&instructions);
+    let origin = (0, 0);
+    // maybe have to center on origin? lets try keeping track of just points right now...
+    let mut boundary = BTreeSet::new();
+    boundary.insert(origin);
+    let mut cur = origin;
+    instructions.iter().for_each(|i| {
+        let d: (i32, i32) = match i.direction {
+            D::R => (1, 0),
+            D::U => (0, -1),
+            D::D => (0, 1),
+            D::L => (-1, 0),
+        };
+        (1..=i.steps as i32).for_each(|step| {
+            let next_hole = (cur.0 + step * d.0, cur.1 + step * d.1);
+            // dbg!(&next_hole);
+            boundary.insert(next_hole);
+        });
+        cur = (cur.0 + i.steps as i32 * d.0, cur.1 + i.steps as i32 * d.1);
+    });
+
+    let insides = fill(&boundary, (1, 1));
+    display(&boundary, &insides);
+    Some(boundary.len() as u32 + insides.len() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -89,7 +171,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(62));
     }
 
     #[test]
