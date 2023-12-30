99d783593248b1820539bf3df20e339cec03bbb7
Steven Lotito <steven.lotito@gmail.com>
2023-12-30 10:44:25 -0800

    day 11, part 2 and refactored part 1

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,93 +1,90 @@
+use std::collections::BTreeSet;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(11);
 
-pub fn parse(input: &str) -> Vec<Vec<char>> {
+fn manhattan(a: &(u32, u32), b: &(u32, u32)) -> i64 {
+    i64::abs(b.0 as i64 - a.0 as i64) + i64::abs(b.1 as i64 - a.1 as i64)
+}
+
+fn parse(input: &str) -> Vec<(u32, u32)> {
     input
         .lines()
-        .map(|line| line.chars().collect())
-        .collect_vec()
-}
-
-// now that I look at this, it probably would have been easier to keep a string, or maybe convert to a bitmap of sorts
-pub fn expand(image: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut expanded = vec![];
-    image.iter().for_each(|row| {
-        if row.iter().filter(|c| **c != '.').count() == 0 {
-            expanded.push(row.clone());
-        }
-        expanded.push(row.clone());
-    });
-    let mut delta = 0; // going to have to keep track of how many cols we added
-    (0..image[0].len()).for_each(|col| {
-        let mut row = 0;
-        loop {
-            if row == image.len() {
-                // this row needs to be duplicated
-                (0..expanded.len()).for_each(|row| {
-                    expanded[row].insert(col + delta, '.');
-                });
-                delta += 1;
-                break;
-            } else if image[row][col] == '#' {
-                // no need to expand this col
-                break;
-            }
-            row += 1;
-        }
-    });
-    expanded
-}
-
-fn points_from(image: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
-    image
-        .iter()
         .enumerate()
-        .map(|(x, row)| {
-            row.iter()
-                .enumerate()
-                .filter_map(|(y, ch)| {
-                    if *ch == '#' {
-                        Some((x.clone(), y))
+        .map(|(y, line)| {
+            line.char_indices()
+                .map(|(x, ch)| {
+                    if ch == '#' {
+                        Some((x as u32, y as u32))
                     } else {
                         None
                     }
                 })
                 .collect_vec()
         })
-        .flatten()
+        .flat_map(|v| {
+            v.iter()
+                .filter(|p| p.is_some())
+                .map(|pt| pt.unwrap())
+                .collect_vec()
+        })
         .collect_vec()
 }
 
+fn expand(points: &mut Vec<(u32, u32)>, max_x: u32, max_y: u32, delta: u32) {
+    let rows = points.iter().map(|pt| pt.1).collect::<BTreeSet<u32>>();
+    let cols = points.iter().map(|pt| pt.0).collect::<BTreeSet<u32>>();
+
+    let expand_rows = (0..=max_y).filter(|row| !rows.contains(row)).collect_vec();
+    let expand_cols = (0..=max_x).filter(|col| !cols.contains(col)).collect_vec();
+
+    expand_rows.iter().enumerate().for_each(|(n, row_num)| {
+        points.iter_mut().for_each(|pt| {
+            if pt.1 >= (n as u32 * delta) + row_num {
+                pt.1 += delta;
+            }
+        });
+    });
+
+    expand_cols.iter().enumerate().for_each(|(n, col_num)| {
+        points.iter_mut().for_each(|pt| {
+            if pt.0 >= (n as u32 * delta) + col_num {
+                pt.0 += delta;
+            }
+        });
+    });
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let image = parse(input);
-    let expanded = expand(image);
-    let points = points_from(&expanded);
-    // let n = points.len();
-    // println!(
-    //     "we will have to look at {} unique pairs of points",
-    //     n * (n - 1) / 2
-    // );
+    let max_y = input.lines().count() as u32;
+    let max_x = input.lines().nth(0).unwrap().chars().count() as u32;
+
+    let mut points = parse(input);
+    expand(&mut points, max_x, max_y, 1);
+
     let unique_points = points.iter().combinations(2).collect_vec();
+
     let result = unique_points
         .iter()
-        .map(|pt| {
-            // println!(
-            //     "need to calculate  distance between {:?} and {:?}",
-            //     pt[0], pt[1]
-            // );
-            manhattan(pt[0], pt[1]) as u32
-        })
+        .map(|pt| manhattan(pt[0], pt[1]) as u32)
         .sum();
+
     Some(result)
 }
 
-fn manhattan(pt1: &(usize, usize), pt2: &(usize, usize)) -> i32 {
-    i32::abs(pt1.0 as i32 - pt2.0 as i32) + i32::abs(pt1.1 as i32 - pt2.1 as i32)
-}
+pub fn part_two(input: &str) -> Option<u64> {
+    let max_y = input.lines().count() as u32;
+    let max_x = input.lines().nth(0).unwrap().chars().count() as u32;
+    let mut points = parse(input);
+    expand(&mut points, max_x, max_y, 1000000 - 1); // sigh, off by one
+    let unique_points = points.iter().combinations(2).collect_vec();
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+    let result: u64 = unique_points
+        .iter()
+        .map(|pt| manhattan(pt[0], pt[1]) as u64)
+        .sum();
+    Some(result)
 }
 
 #[cfg(test)]
@@ -103,6 +100,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        println!("{:?}", result);
     }
 }
